422cec0f412b1fbc695a0223e519fcc61d27fbd9
sompal <sompal@visao.io>
2026-02-04 18:51:17 +0530

    fix of mobile and responsive

--- src/components/FeaturedSection/index.js
+++ src/components/FeaturedSection/index.js
@@ -5,45 +5,44 @@ import Image from "next/image";
 import React from 'react'
 import FeaturedSmallCard from '../FeaturedSmallCard'
 
-const FeaturedSection = ({editorials}) => {
-   const [selectedIndex, setSelectedIndex] = useState(0);
+const FeaturedSection = ({ editorials }) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
     <div className='pt-8 bg-primary w-full'>
-    <div className='w-full rounded-t-[20px] rounded-r-[20px] bg-white -mt-4'>
-      <div className='p-5 lg:px-[120px] sm:px-[70px] sm:py-16'>
-        <div className='flex flex-row gap-4 flex-wrap'>
-          {editorials.slice(0,4).map((editorial, index)=>(
-            <FeaturedSmallCard key={index} imgPath={editorial.headerImage} slug={editorial.slug} title={editorial.title} desc={editorial.description} />
-          ))}
-        </div>
-       <div className="flex flex-row sm:mt-10 mt-5 rounded-[20px]">
-        {editorials.slice(0,4).map((editorial, index)=>(
-                <motion.div
+      <div className='w-full rounded-t-[20px] rounded-r-[20px] bg-white -mt-4'>
+        <div className='p-5 lg:px-[120px] sm:px-[70px] sm:py-16'>
+          <div className='flex flex-row gap-4 flex-wrap'>
+            {editorials.slice(0, 4).map((editorial, index) => (
+              <FeaturedSmallCard key={index} imgPath={editorial.headerImage} slug={editorial.slug} title={editorial.title} desc={editorial.description} />
+            ))}
+          </div>
+          <div className="flex flex-row sm:mt-10 mt-5 rounded-[20px] overflow-hidden">
+            {editorials.slice(0, 4).map((editorial, index) => (
+              <motion.div
                 key={index}
-                className={`relative flex-grow transition-all duration-300 sm:h-[512px] h-[129px] hover:cursor-pointer  ${
-                  selectedIndex === index ? "col-span-3" : "col-span-1"
-                }`}
+                className="relative flex-grow sm:h-[512px] h-[129px] hover:cursor-pointer bg-no-repeat bg-cover bg-center"
                 onHoverStart={() => setSelectedIndex(index)}
-                onHoverEnd={()=>setSelectedIndex(-1)}
+                onHoverEnd={() => setSelectedIndex(-1)}
                 style={{
-                  flex: selectedIndex === index ? "5" : "1",
+                  // flex: selectedIndex === index ? "5" : "1",
+                  backgroundImage: `url(${editorial.headerImage.src})`,
                 }}
-              >
-                <Image
-                  src={editorial.headerImage}
-                  alt={editorial.title}
-                  className={`w-full h-full ${
-                    selectedIndex === index
-                      ? "object-cover w-full"
-                      : "object-cover sm:w-auto  w-[49px]"
-                  } sm:h-full h-[128px]`}
-                />
-                </motion.div>
-          ))}
-       </div>
-      </div>
+                initial={false}
+                animate={{
+                  backgroundPosition:
+                    selectedIndex === index ? "100% center" : "0% center",
+                }}
+                transition={{
+                  duration: 0.8,
+                  ease: "easeOut",
+                }}
+              />
 
-    </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
     </div>
 
   )
