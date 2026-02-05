4fb08f556ae49fcc81993f93c62988f57f5da823
sompal <sompal@visao.io>
2026-02-05 16:55:05 +0530

    not found changes

--- src/components/NotFoundComponent/index.js
+++ src/components/NotFoundComponent/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import notFound from "../../static/images/editorial/not_found.png"
+import notFound from "../../static/images/editorial/not_found.svg"
 import Link from 'next/link'
 import { Plus_Jakarta_Sans } from "next/font/google";
 
@@ -11,16 +11,36 @@ const jarkata = Plus_Jakarta_Sans({
 
 const NotFoundComponent = () => {
   return (
-    <div className='w-full relative p-5 lg:px-[120px] sm:px-[70px] sm:py-16 z-1'>
-        <Image className='w-full h-full object-contain img-responsive' src={notFound} alt='not found' width={926} height={450} />
-        <div className={`flex flex-col gap-8 ${jarkata.className} max-w-[550px] mx-auto mt-10`}>
-            <h2 className={`text-center text-[28px] leading-[34px] font-bold`}>Yikes , This is awkward</h2>
-            <p className='text-center text-base leading-tight'>The page you’re looking for appears to have been moved, deleted, or doesn’t exist.  We apologize for the inconveniences</p>
-            <Link className='px-4 py-4 bg-green-1 text-white rounded-full text-center max-w-[130px] mx-auto' href="/">
-            Go Home
-            </Link>
-        </div>
-    </div>
+   <div className="relative w-full min-h-screen p-5 lg:px-[120px] sm:px-[70px] sm:py-16 flex flex-col items-center justify-center z-10">
+  <div className="w-full max-h-[45vh] flex justify-center">
+    <Image
+      src={notFound}
+      alt="not found"
+      className="w-full h-full max-h-[45vh] object-contain"
+      width={926}
+      height={450}
+      priority
+    />
+  </div>
+  <div
+    className={`flex flex-col gap-6 ${jarkata.className} max-w-[550px] mx-auto mt-6`}
+  >
+    <h2 className="text-center text-[28px] leading-[34px] font-bold">
+      Yikes, This is awkward
+    </h2>
+    <p className="text-center text-base leading-tight">
+      The page you’re looking for appears to have been moved, deleted, or doesn’t exist.
+      We apologize for the inconvenience.
+    </p>
+    <Link
+      href="/"
+      className="px-4 py-3 bg-green-1 text-white rounded-full text-center max-w-[130px] mx-auto"
+    >
+      Go Home
+    </Link>
+  </div>
+</div>
+
   )
 }
 
