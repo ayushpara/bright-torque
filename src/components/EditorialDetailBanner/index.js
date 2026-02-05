c0aef6b300735fa901bb3e6086abd81889490056
sompal <sompal@visao.io>
2026-02-05 17:25:13 +0530

    breadcrumb

--- src/components/EditorialDetailBanner/index.js
+++ src/components/EditorialDetailBanner/index.js
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const EditorialDetailBanner = ({ editorial }) => {
-  const breadcrumb = `Home  / Reading lists  / ${editorial.title}`
+  const breadcrumb = <div><Link href="/" prefetch className="text-[#212121CC]">Home</Link>  / <Link href="/editorial" prefetch className="text-[#212121CC]">Editorial</Link>  / <Link href={editorial.slug} prefetch>{editorial.title}</Link></div>
   return (
     <div className='w-full relative p-5 lg:px-[120px] sm:px-[70px] sm:py-16 z-1'>
       <div className='absolute top-0 left-0 right-0 h-[65%] bottom-0  bg-primary z-0'></div>
