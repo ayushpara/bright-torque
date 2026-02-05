1a0cb3c03ad60c44bd5badd20f050354b58d2165
sompal <sompal@visao.io>
2026-02-05 19:35:23 +0530

    fixes of trello

--- src/components/YouMightAlsoLike/index.js
+++ src/components/YouMightAlsoLike/index.js
@@ -12,7 +12,7 @@ const YouMightAlsoLike = () => {
         <div className='h-1 bg-[#F2F2F2] w-[100%] my-5' />
       </div>
       <div className="p-5 !pr-0 lg:pl-[120px] sm:pl-[70px] sm:py-8">
-        <div className='flex flex-row overflow-x-auto gap-4 no-scrollbar'>
+        <div className='flex flex-row overflow-x-auto gap-4 no-scrollbar pr-8'>
           {editorials.map((editorial, index) => (
             <div className='min-w-[45%]' key={index}>
               <EditorialSquareCard imgPath={editorial.headerImage} slug={editorial.slug} title={editorial.title} desc={editorial.description} by={editorial.by} />
