ce0acd6b1617e879fdb083eae33e83815ac5dcbe
sompal <sompal@visao.io>
2026-02-06 20:13:53 +0530

    detail banner

--- src/components/YouMightAlsoLike/index.js
+++ src/components/YouMightAlsoLike/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import EditorialSquareCard from '../EditorialSquareCard'
 import { editorials } from '@/data/editorials'
+import HeadingWithRuler from '../HeadingWithRuler'
 
 const YouMightAlsoLike = () => {
   return (
     <div className='mb-8'>
       <div className="p-5 lg:px-[120px] sm:px-[70px] sm:py-8">
-       <h3 className="text-[#001325F2] font-medium text-[44px] leading-[44px] mb-6">
-          You Might Also Like
-        </h3>
-        <div className='h-1 bg-[#F2F2F2] w-[100%] my-5' />
+        <HeadingWithRuler title="You Might Also Like" />
       </div>
       <div className="p-5 !pr-0 lg:pl-[120px] sm:pl-[70px] sm:py-8">
         <div className='flex flex-row overflow-x-auto gap-4 no-scrollbar pr-8'>
