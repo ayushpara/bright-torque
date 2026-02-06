0e52dd64636c96d153d2b70c55d7f2c4104f9618
sompal <sompal@visao.io>
2026-02-06 20:33:01 +0530

    editor card

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,6 +7,8 @@ import Image from 'next/image';
 import { Plus_Jakarta_Sans, Syne } from 'next/font/google';
 import Link from 'next/link';
 import { headerButtons } from '@/data';
+import { useRouter } from 'next/navigation';
+import { CONTACT, HOME } from '@/contants/routes';
 const inter = Syne({ subsets: ["latin"] });
 const jarkata = Plus_Jakarta_Sans({
   weight: "400",
@@ -14,6 +16,7 @@ const jarkata = Plus_Jakarta_Sans({
 });
 const Footer = () => {
     const date = new Date()
+    const router = useRouter()
     return (
         <div className={`p-5 lg:px-[120px] sm:px-[70px] sm:py-16 ${inter.className} bg-[#033941] text-white`}>
             <div className="flex flex-col sm:flex-row justify-between items-center">
@@ -26,9 +29,9 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className='w-full sm:w-auto mt-6 sm:mt-0'>
-                    <Link href={`mailto:example@gmail.com`} className='text-green-1 bg-secondary w-auto py-6 h-9 rounded-full text-center flex items-center justify-center bg-white text-base font-semibold leading-normal mt-2 w-full sm:w-[183px]'>
+                    <button  onClick={()=> router.push(CONTACT,{ scroll: true })} className='text-green-1 bg-secondary w-full px-6 py-6 h-9 rounded-full text-center flex items-center justify-center bg-white text-base font-semibold leading-normal mt-2 w-full sm:w-[183px]'>
                         Chat with us <Image src={Arrow} width={20} height={20} className='ml-2' />
-                    </Link>
+                    </button>
                 </div>
             </div>
             <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center mt-8'>
