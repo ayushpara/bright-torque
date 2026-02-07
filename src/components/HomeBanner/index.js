5d0a1ca8bbcba3495e230d69fa2bc67f2a670fb3
sompal <sompal@visao.io>
2026-02-07 16:16:41 +0530

    stats

--- src/components/HomeBanner/index.js
+++ src/components/HomeBanner/index.js
@@ -21,16 +21,13 @@ const HomeBanner = () => {
     <div className="w-full bg-primary">
       <div className="p-5 lg:px-[120px] sm:px-[70px] sm:py-16">
         <div className="w-full grid flex-row sm:grid-cols-4">
-          <div className="sm:col-span-3 flex flex-col order-last sm:order-none">
+          <div className="sm:col-span-4 flex flex-col order-last sm:order-none">
             <p className="sm:text-[72px] text-[31px] sm:leading-[84px] leading-[40px] text-green-1 ">
               Design Experts.
             </p>
             <p className="sm:text-[72px] text-[31px]  sm:leading-[84px] leading-[40px] text-secondary text-green-2 ">
               Design Decisions, Made Visible.
             </p>
-            {/* <p className="sm:text-[72px] text-[31px]  sm:leading-[84px] leading-[40px] text-green-1 ">
-              Zero-to-One Startups.
-            </p> */}
             <p
               className={`text-green-3 sm:text-xl text-base mt-5 ${jarkata.className} sm:block hidden `}
             >
@@ -41,35 +38,6 @@ const HomeBanner = () => {
             >
               We’re a Product Design & Branding<br /> Agency  who specializes in B2B SaaS,<br /> Ecommerce platforms, complex web apps and<br /> diverse UI/UX design projects.
             </p>
-          </div>
-          <div className="flex flex-row justify-end items-center">
-            <motion.div
-              initial="initial"
-              animate="animate"
-              variants={rotateVariants}
-              className="sm:col-span-1 justify-end items-center flex sm:h-[123px] sm:w-[123px] w-[45px] h-[45px]"
-              style={{ transformOrigin: "center center" }}
-            >
-              <motion.div
-                className="absolute  "
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: "50%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Image
-                  style={{ transformOrigin: "center center" }}
-                  src={starIcon}
-                  alt="star"
-                  fill
-                  className="object-cover absolute w-full h-full"
-                />
-              </motion.div>
-            </motion.div>
           </div>
         </div>
       </div>
