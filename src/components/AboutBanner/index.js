1a7b358113ed38a8d1c9c7d87df6af2f654ad37c
sompal <sompal@visao.io>
2026-02-17 09:38:37 +0530

    about us page and home accordion

--- src/components/AboutBanner/index.js
+++ src/components/AboutBanner/index.js
@@ -18,10 +18,10 @@ const AboutBanner = () => {
     <div className="w-full bg-primary">
       <div className="p-5 lg:px-[120px] sm:px-[70px] sm:py-16">
         <div className="w-full sm:flex-row flex-col flex ">
-          <div className="flex flex-col order-last sm:order-none sm:w-3/4">
-            <h1 className={`sm:text-[72px] text-[35px]  sm:leading-[74px] leading-[40px] text-green-1 ${syne.className}`}>
+          <div className="flex flex-col order-last sm:order-none sm:w-3/4 lg:pr-20">
+            <h1 className={`sm:text-[72px] text-[35px]  sm:leading-[74px] leading-[40px] text-secondary ${syne.className}`}>
               Transforming your ideas into{" "}
-              <span className="text-secondary">Brand & Products</span>
+              <span className="text-green-1">Brand & Products</span>
             </h1>
             <h1 className={`text-green-3 sm:text-xl text-base sm:pt-5 pt-3 ${jarkata.className} `}>
               Breaking new ground to transform the industry.
